3bcffed917a6a0953d21e947a75c17740000a6bd
Agnibesh M <me@agnibesh.dev>
2020-05-17 15:11:06 +0000

    my life

--- src/App.js
+++ src/App.js
@@ -8,12 +8,13 @@ function App() {
   const [meals, setMeals] = useState([]);
   const [neww,setNeww] = useState(true);
   const newwHandler = () => setNeww(!neww);
+
   const mealEffectHandler = (meal) => setMeals([meal]);
   useEffect(()=>{
     axios.get(`https://www.themealdb.com/api/json/v1/1/random.php`)
       .then(res => {
         mealEffectHandler(res.data.meals[0]);
-        console.log(res.data.meals[0])
+        // console.log(res.data.meals[0])
       })
   },[neww])
 
@@ -23,15 +24,21 @@ function App() {
         <h1 className="head">Agnibesh's Random Recipe?</h1>
         <button className="loadmeal" onClick={newwHandler}>Load new Meal</button>
         <ul>
-          {meals.map((meal)=> <li key={uuidv4()}>
-            <h1>{meal.strMeal}</h1>
+          {meals.map((meal)=>{
+            let para = meal.strInstructions.split("↵");
+            let finpara = para[0].split("↵↵");
+            return(
+              <li key={uuidv4()}>
+            <h1>{meal.strMeal}</h1><a href={meal.strYoutube}>yt</a>
             <hr/>
-            <p>{meal.strInstructions}</p>
-          </li>)}
+            <p>{para}</p>
+          </li>
+            )
+          })}
         </ul>
       </div>
       <footer>
-        hi
+        <a href="https://www.agnibesh.dev">© Agnibesh Mukherjee 2020</a>
       </footer>
     </div>
   );
